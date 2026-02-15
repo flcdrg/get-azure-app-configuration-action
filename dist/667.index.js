b42d91bb5639c070257398f8756af297d377d558
David Gardiner <david@gardinerfamily.au>
2026-02-15 21:59:17 +1030

    Update files

--- dist/667.index.js
+++ dist/667.index.js
@@ -292,7 +292,7 @@ async function default_browser_defaultBrowser() {
 
 
 // Path to included `xdg-open`.
-const open_dirname = external_node_path_.dirname((0,external_node_url_.fileURLToPath)(import.meta.url));
+const open_dirname = external_node_path_.dirname((0,external_node_url_.fileURLToPath)(require("url").pathToFileURL(__filename).href));
 const localXdgOpenPath = external_node_path_.join(open_dirname, 'xdg-open');
 
 const {platform, arch} = external_node_process_;
