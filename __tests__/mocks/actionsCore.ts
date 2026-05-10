ba3194ece96a2a389dd05305a0e5979ad232eb37
David Gardiner <david@gardinerfamily.au>
2026-05-10 20:34:38 +0930

    Migrate to ESM and Node 24
    - Switch to rimraf/rollup

---
+++ __tests__/mocks/actionsCore.ts
@@ -0,0 +1,25 @@
+export function setSecret(secret: string): void {
+  void secret;
+}
+export function debug(message: string): void {
+  void message;
+}
+export function setOutput(name: string, value: string | undefined): void {
+  void name;
+  void value;
+}
+export function exportVariable(name: string, value: string | undefined): void {
+  void name;
+  void value;
+}
+export function setFailed(message: string): void {
+  void message;
+}
+export function getInput(
+  name: string,
+  options?: { required?: boolean }
+): string {
+  void name;
+  void options;
+  return '';
+}
